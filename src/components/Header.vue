d54a7a4f736b41c5b9c98fac798e0b3d1bad8896
Bruno-Felix <balvesfelix@gmail.com>
2020-09-25 19:18:51 -0300

    #104 Inserindo view - CriarPostagem | Inserindo components - Header e MenuBar

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+            <!-- <img id="wordmark" src="../assets/Wordmark.png"> -->
+    </header>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+    header{
+        height: 45px;
+        width: 100%;
+        background-color: #090673;
+    }
+</style>
+
---
+++ src/views/CriarPostagem.vue
@@ -0,0 +1,21 @@
+<template>
+    <Header/>
+    <div class="criarPostagem">
+        <h1>Página: CriarPostagem</h1>
+    </div>
+</template> 
+
+<script>
+
+import Header from '@/components/Header.vue'
+export default {
+    name: 'CriarPostagem',
+    components: {
+    Header
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
