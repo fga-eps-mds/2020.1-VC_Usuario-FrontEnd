8851578128be8220f4c658de1c02eeb7594e10c7
Bruno-Felix <balvesfelix@gmail.com>
2020-09-25 19:16:51 -0300

    #104 Retirando páginas padrão de início do vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,19 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view/>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+
+html {
+  margin: 0;
+}
+body {
+  margin: 0;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+#app {
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+  font-family: 'Montserrat', sans-serif;
+  color: #000000;
 }
 </style>
