78533b4fbb56a2d1c593f6e20170f401c698a692
Bruno-Felix <balvesfelix@gmail.com>
2020-09-26 20:03:29 -0300

    #104 Importando components Header e MenuBar | Modificando style da Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,43 @@
 <template>
-  <h1>Página: Feed de Postagens</h1>
+  <Header/>
+    <div class="feedPostagens">
+        <h1>Página: Feed dee Postagens</h1>
+        <h1>Página: Feed dee Postagens</h1>
+        <h1>Página: Feed dee Postagens</h1>
+        <h1>Página: Feed dee Postagens</h1>
+        <h1>Página: Feed dee Postagens</h1>
+        <h1>Página: Feed dee Postagens</h1>
+        <h1>Página: Feed dee Postagens</h1>
+        <h1>Página: Feed dee Postagens</h1>
+        <h1>Página: Feed dee Postagens</h1>
+        <h1>Página: Feed dee Postagens</h1>
+        <h1>Página: Feed dee Postagens</h1>
+        <h1>Página: Feed dee Postagens</h1>
+        <h1>Página: Feed dee Postagens</h1>
+        <h1>Página: Feed dee Postagens</h1>
+        <h1>Página: Feed dee Postagens</h1>
+    </div>
+    <MenuBar/>
 </template>
 
 <script>
+
+/* Import dos components */
+import Header from '@/components/Header.vue'
+import MenuBar from '@/components/MenuBar.vue'
+export default {
+    name: 'Home',
+    components: {
+    Header,
+    MenuBar
+  }
+}
 
 </script>
 
 <style scoped lang="scss">
 
+    .feedPostagens{
+        margin-top: 60px;
+    }
 </style>
