1c3b4165a06d3683bf3b4ba69d38519a6ee13dd8
Bruno-Felix <balvesfelix@gmail.com>
2020-09-26 19:58:58 -0300

    #104 Criando comportanmento do component Header em relação ao scroll da tela | Modificando style do component da Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,39 @@
 <template>
-    <header>
-            <!-- <img id="wordmark" src="../assets/Wordmark.png"> -->
+    <header id="header">
+        <!-- <img id="wordmark" src="../assets/Wordmark.png"> -->
     </header>
 </template>
 
 <script>
-export default {
+
+/* Script do scroll da Header */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function(){
+
+    var currentScrollpos = window.pageYOffset;
     
+    if(prevScrollpos > currentScrollpos){
+        document.getElementById("header").style.top = "0px";
+    }else{
+        document.getElementById("header").style.top = "-100px"
+    }
+
+    prevScrollpos = currentScrollpos;
 }
+
 </script>
 
 <style lang="scss" scoped>
 
     header{
-        height: 45px;
+        top: 0;
+        display: block;
+        position: fixed;
+
+        height: 50px;
         width: 100%;
+        
+        transition: top 0.3s;
         background-color: #090673;
     }
 </style>
