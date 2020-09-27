a2cc4813103933edaf0c5829f5b11f5c5b02a1b0
Bruno-Felix <balvesfelix@gmail.com>
2020-09-27 03:28:01 -0300

    #104 Inserindo e estilizando os 4 Icons na MenuBar

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -1,6 +1,11 @@
 <template>
     <nav>
-
+        <div class="divMenuOpcoes">
+            <div><img src="../assets/homeIcon.png"> </div>
+            <div><img src="../assets/addIcon.png"> </div>
+            <div><img src="../assets/newsIcon.png"> </div>
+            <div><img src="../assets/userIcon.png"> </div>
+        </div>
     </nav>
 </template>
 
@@ -13,11 +18,34 @@
     nav{
         bottom: 0;
         position: fixed;
+        display: flex;
 
-        height: 70px;
+        height: 85px;
         width: 100%;
 
         background-color: #ffffff;
         border-top: 1px solid #DADDE0;
-    }
+    }   
+
+    .divMenuOpcoes{
+        display: flex;
+        width: 100%;
+        margin: 10px 20px;
+
+        & div{
+            display: flex;
+            justify-content: center;
+
+            height: 100%;
+            flex: 1;
+            
+            & img{
+                display: block;
+
+                margin: auto;
+                height: 30px;
+                padding: 15px 15px;
+            }
+        }
+    }   
 </style>
