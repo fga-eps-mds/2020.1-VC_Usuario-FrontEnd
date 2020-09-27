3213d10d01137145a82670c3ac772d92da386941
Bruno-Felix <balvesfelix@gmail.com>
2020-09-27 03:26:57 -0300

    #104 Inserindo e estilizando Wordmark na Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,8 @@
 <template>
     <header id="header">
-        <!-- <img id="wordmark" src="../assets/Wordmark.png"> -->
+        <div class="divWordmark">
+            <img src="../assets/Wordmark.png"> 
+       </div>
     </header>
 </template>
 
@@ -27,14 +29,31 @@ window.onscroll = function(){
 
     header{
         top: 0;
-        display: block;
         position: fixed;
+        display: flex;
 
-        height: 50px;
+        height: 45px;
         width: 100%;
         
         transition: top 0.3s;
         background-color: #090673;
+    }
+
+    .divWordmark{
+        display: flex;
+        justify-content: center;
+
+        height: 100%;
+        width: 100px;
+        margin: auto;
+
+        & img{
+            display: block;
+
+            height: 35px;
+            margin: auto;
+            padding: 0 10px;
+        }
     }
 </style>
 
