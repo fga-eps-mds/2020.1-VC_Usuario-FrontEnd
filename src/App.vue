985067cf054f0c751344cd5b736782efb1236a8d
Bruno-Felix <balvesfelix@gmail.com>
2020-09-30 18:44:24 -0300

    #104 Afinando o style base do projeto

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,31 @@
 
 <style lang="scss">
 
-    html {
+    * {
         margin: 0;
+        padding: 0;
+        outline: 0;
+        /* box-sizing: border-box; */
+
+        @import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+        font-family: 'Montserrat', sans-serif;
     }
+
+    html, body, #app{
+        margin: 0;
+        height: 100%;
+    }
+
     body {
         margin: 0;
-    }
-    h1, h2, h3, h4 {
-        margin: 0;
+        width: 100%; 
+    
+        text-rendering: optimizeLegibility;
+        -webkit-font-smoothing: antialiased;
+
+        color: #000000;
+        font-size: 16px;
+        background-color: #ffffff;
     }
 
     select, input{
@@ -21,9 +38,9 @@
         outline: 0;
     }
 
-    #app {
-        @import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
-        font-family: 'Montserrat', sans-serif;
-        color: #000000;
-    }
+    //Cores da Identidade Visual do Produto:
+        $colorAzul: #090673;
+        $colorVermelho: #CE1335;
+        $colorVerde: #248722;
+        $colorCinza: #DADDE0;
 </style>
