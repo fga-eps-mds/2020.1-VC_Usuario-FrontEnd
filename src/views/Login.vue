f1fb240165e260aae993c71137cb8ba8ec0bf3e4
Bruno-Felix <balvesfelix@gmail.com>
2020-10-04 18:29:45 -0300

    #115 Afinando e ajustando tamando dos elementos da página para serem responsivos a altura da tela - Login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,23 +1,25 @@
 <template> 
-        <div class="divLogin">
-            <div class="botaoVoltar">
-                 <!-- <img src="../assets/wordmarkIcon.png"> --> 
+    <div class="divLogin">
+        <div class="botaoVoltar">
+            
+        </div>
+
+        <div class="divLogo">
+            <div class="logo">
+
             </div>
-            <div class="divLogo">
-                <div class="Logo">
+        </div>  
 
-                </div>
-            </div>  
-            <form action="">
-                <div class="inputFields">
-                    
-                </div>
+        <form action="">
+            <div class="inputCampos">
+                
+            </div>
 
-                <div class="botoesOpcoes">
+            <div class="botoesOpcoes">
 
-                </div>
-            </form>    
-        </div>    
+            </div>
+        </form>
+    </div>  
 </template> 
 
 <script>
@@ -26,57 +28,53 @@
 </script>
 
 <style lang="scss" scoped>
-    template{
-        height: 100%;
-        border: 1px solid blue;
-    }
     .divLogin{
-        border: 1px solid red;
+        /* border: 1px solid red; */
         height: 100%;
-        padding: 30px;
-        display: block;
-        & form{
-            width: 100%;
-            height:450px;
-            margin-top: 20px;
-            border: 1px solid red;
-            & .inputFields{
-                width: 100%;
-                height: 215px;
-                border: 1px solid black;
-            }
-            & .botoesOpcoes{
-                width: 100%;
-                height: 215px;
-                margin-top: 20px;
-                border: 1px solid black;
-            }
-        }
+        max-width: 600px;
+        min-height: 600px;
+        margin: 0 30px;
+        padding: 0;
     }
+
     .botaoVoltar{
         border: 1px solid blue;
-        height: 40px;
+        height: 5%;
         top: 0;
         left: 0;
         width: 40px;
     }
+
     .divLogo{
         width: 100%;
-        height: 200px;
-        margin-top: 20px;
+        height: 30%;
+        margin-top: 2%;
         display: flex;
         border: 1px solid blue;
+
+        & .logo{
+            width: 150px;
+            height: 100px;
+            margin: auto;
+            border: 1px solid red;
+        }
     }
-    .Logo{
-        width: 200px;
-        height: 100px;
-        margin: auto;
-        border: 1px solid red;
-    }
+
     form{
-
+        width: 100%;
+        height: 50%;
+        margin-top: 2%;
+        border: 1px solid green;
+        & .inputCampos{
+            width: 100%;
+            height: 45%;
+            border: 1px solid black;
+        }
+        & .botoesOpcoes{
+            width: 100%;
+            height: 45%;
+            margin-top: 5%;
+            border: 1px solid black;
+        }
     }
-
-    
-
 </style>
