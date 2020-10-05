89e16ec9b253684cdeb6de0648b2aa37d52b3c6a
Emily Sousa <diassousa@yahoo.com.br>
2020-10-05 12:52:27 -0300

    #116 colocando os componentes de acordo com o protótipo

--- src/views/ListarPostagem.vue
+++ src/views/ListarPostagem.vue
@@ -3,26 +3,35 @@
     <Header/>
 
     <section v-for="postagem of postagens" :key="postagem._id">
-        <div class="PostagemTitulo">
+        <div class="postagemTitulo">
             <h1>{{postagem.post_title}}</h1>
         </div>
 
-        <div class="PostagemLugar">
+        <div class="postagemLugar">
             <h2>{{postagem.post_place}}</h2>
         </div>
 
-        <div class="PostagemData">
+        <div class="postagemData">
             <h2>{{postagem.post_created_at}}</h2>
         </div>
 
-        <div class="PostagemDescricao">
-            Descrição:
-            <p>{{postagem.post_description}}</p>
+        <div class="postagemCategoria">
+            <h2>{{postagem.post_category}}</h2>
         </div>
 
-        <div class="PostagemBotoes">
-                <button type="submit">Comentar</button>
-                <button type="submit">Cancelar</button>
+        <div class="postagemDescricao">
+            Descrição:
+            <p>{{postagem.post_description}}</p>
+            <button type="submit" class="apoiarButton">Apoiar</button>
+        </div>
+
+        <div class="postagemComentario">
+            Comentários:
+        </div>
+
+        <div class="postagemBotoes">
+            <button type="submit">Comentar</button>
+            <button type="submit">Reportar</button>
         </div>
     </section>
 
@@ -62,7 +71,7 @@ export default {
 <style scoped lang="scss">
 @import "../assets/stylesheets/pallete.scss";
 
-    .PostagemTitulo{
+    .postagemTitulo{
         margin-top: 60px;
         margin-left: 5%;
         max-width: 200px;
@@ -70,26 +79,50 @@ export default {
         color: $colorPreta;
     }
 
-    .PostagemLugar{
+    .postagemLugar{
         font-size: 10px;
         margin-left: 5%;
         margin-top: 5%;
     }
 
-    .PostagemData{
+    .postagemData{
         font-size: 10px;
         margin-left: 5%;
     }
 
-    .PostagemDescricao{
-        margin-top: 20%;
-        margin-left: 5%
+    .postagemData{
+        font-size: 10px;
+        margin-left: 5%;
     }
 
-    .PostagemBotoes{
+    .postagemCategoria{
+        font-size: 10px;
+        margin-left: 5%;
+    }
+
+    .postagemComentario{
+        margin-top: 20%;
+        margin-left: 5%;
+        height: 100px;
+    }
+
+    .postagemDescricao{
+        margin-top: 20%;
+        margin-left: 5%;
+        height: 100px;
+    }
+
+    .apoiarButton{
+        margin-right: 20px;
+        color: $colorBranca;
+        background-color: $colorAzul;
+        font-size: 20px;
+        border: none;
+        border-radius: 10px;
+    }
+
+    .postagemBotoes{
         display: flex;
-        
-        /* border: 1px solid blue; */
 
         & button{
             flex: 1;
@@ -101,7 +134,6 @@ export default {
 
         button:first-child{
             margin-right: 20px;
-            
             color: $colorBranca;
             background-color: $colorAzul;
         }
