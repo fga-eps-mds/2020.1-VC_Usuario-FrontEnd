7d9886d8dad573b30909a9c4d1239acc735a9ab3
Bruno-Felix <balvesfelix@gmail.com>
2020-10-13 16:36:55 -0300

    #128 Padronizando nome de variáveis e chamada de elementos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,8 @@
   <Header/>
     <form class="feedPostagens">
       <!-- <h1>{{post.post_title}}</h1> -->
-      <div class="col" v-for="post in this.commonPosts" :key="post.id">
-        <PostBlock v-bind:title="post.post_title"  v-bind:status="post.post_status" author="Daniel Porto" v-bind:local="post.post_place" v-bind:date="post.post_created_at" v-bind:id="post._id" @ver-mais="verMais" />
+      <div class="col" v-for="postagem in this.postagemData" :key="postagem.id">
+        <PostagemComponent v-bind:title="postagem.post_title"  v-bind:status="postagem.post_status" author="Daniel Porto" v-bind:local="postagem.post_place" v-bind:date="postagem.post_created_at" v-bind:id="postagem._id" @ver-mais="verMais"/>
       </div>
       
     </form>
@@ -14,37 +14,39 @@
 <script>
 /* Import dos components */
 import Header from '@/components/Header.vue'
-import PostBlock from '@/components/PostBlock.vue'
 import MenuBar from '@/components/MenuBar.vue'
-import Postage from '@/services/postagens.js'
-//import router from 'vue-router'
+
+import PostagemComponent from '@/components/PostagemComponent.vue'
+
+/* Import dos services */
+import PostagemService from '@/services/postagens.js'
 
 export default {
     name: 'Home',
 
     data (){
       return {
-        commonPosts: { }
+        postagemData: { }
       }
     },
 
     created: function(){
-      this.listPosts();
+      this.listarPostagens();
     },
 
     components: {
       Header,
-      PostBlock,
-      MenuBar
+      MenuBar,
+      PostagemComponent
     },
 
     methods: {
 
-      listPosts(){
-        Postage.listarPostagem().then(Response => {
+      listarPostagens(){
+        PostagemService.listarPostagem().then(Response => {
           console.log(Response);
-          this.commonPosts = Response.data.posts;
-          console.log(this.commonPosts);
+          this.postagemData = Response.data.posts;
+          console.log(this.postagemData);
         })
       },
 
