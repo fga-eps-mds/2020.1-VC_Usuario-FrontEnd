c2b9434ca8f6d2b6916aae3b6d4c64beb5015fe8
DanielPortods <danielportods@gmail.com>
2020-10-14 19:23:14 -0300

    #130 Instalando dependencias e fazendo pequenas adaptaçoes.

--- src/services/usuarios.js
+++ src/services/usuarios.js
@@ -5,6 +5,7 @@ export default{
     Registrar:(user) => {
         return http.post('user/register_user', user)
     },
+    
     Login:(login) => {
         return http.post('user/login', login)
     },
