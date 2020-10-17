031e787b72634518c59c22baa4deec5f4bcd2728
DanielPortods <danielportods@gmail.com>
2020-10-17 00:00:23 -0300

    #130 Instalando dependencias e configurando o store.

--- src/store/index.js
+++ src/store/index.js
@@ -1,39 +1,52 @@
-import { reactive } from 'vue';
+import { createStore, createLogger } from 'vuex';
+import createPersistedState from "vuex-persistedstate";
 
-export const store = {
+const store = createStore({
     debug: true,
-    state: reactive({
-        usuario: {},
-        token: {}
-    }),
-
-    setUsuarioAction(novoUsuario) {
-        if(this.debug){
-            console.log('Novo nome: ', novoUsuario)
-        }
-
-        this.state.usuario = novoUsuario;
+    plugins: [createLogger(), createPersistedState()],
+    state(){
+        return {
+            usuario: {},
+            token: {}
+        }        
     },
 
-    setTokenAction(token) {
-        if(this.debug){
-            console.log('Token: ', token)
-        }
+    mutations: {
+        SET_USUARIO(state, payload) {
+            if(this.debug){
+                console.log('Novo nome: ', payload)
+            }
+    
+            state.usuario = payload;
+        },
 
-        this.state.token = token;
     },
 
-    clearNomeAction() {
-        if(this.debug){
-            console.log('Nome limpo')
-        }
-
-        this.state.nome = ''
+    actions: {
+        
+    
+        setTokenAction(token) {
+            if(this.debug){
+                console.log('Token: ', token)
+            }
+    
+            this.state.token = token;
+        },
+    
+        clearNomeAction() {
+            if(this.debug){
+                console.log('Nome limpo')
+            }
+    
+            this.state.nome = ''
+        },
     },
 
-    getNome() {
-        return this.state.usuario.user_name;
-    }
-}
+    getters: {
+        getNome(state) {
+            return state.usuario.user_name;
+        }        
+    }     
+})
 
-export {store as default};
+export default store ;
