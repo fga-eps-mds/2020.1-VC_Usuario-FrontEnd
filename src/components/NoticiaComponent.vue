51c080a418ea10e9b4c5ac1900fdabfe22c68290
Bruno-Felix <balvesfelix@gmail.com>
2020-10-18 20:43:47 -0300

    #139 Criando view FeedDeNoticias e VisualizarNoticia e component NoticiaComponent Co-authored-by: Daniel Porto <danielportods@gmail.com>

---
+++ src/components/NoticiaComponent.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="noticia">
+        <div class="tituloESubTitulo">
+            <span>Titulo</span>
+            <span>SubTitulo</span>
+        </div>
+
+        <div class="divBotoes">
+            <button>Ver mais</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'noticia'
+}
+</script>
+
+<style lang="scss">
+
+    .noticia {
+        height: auto;
+        border-radius: 15px;
+        height: 150px;
+        padding: 10px;
+
+        border: solid 1px #DADDE0;
+    }
+
+    .tituloESubTitulo {
+        height: auto;
+        /* border: 1px solid red; */
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        margin-bottom: 20px;  
+
+        & span:first-child{
+            /* width: 70%; */
+            padding-right: 30px;
+            /* border: 1px solid red; */
+            
+            font-size: 20px;
+            font-weight: bold;
+            color: #090673;
+        }
+    }
+
+    .divBotoes{
+        display: flex;
+        width: 80%;
+        max-width: 300px;
+        margin-bottom: 10px;
+        
+        & button{
+            flex: 1;
+            height: 30px;
+            margin-right: 20px;
+
+            cursor: pointer;
+            border: none;
+            border-radius: 25px; 
+            color: #ffffff;
+            background-color: #090673;
+        }
+
+        & button:hover{
+            background-color: #060449;
+        }
+    }
+</style>
