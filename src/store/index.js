8015a516e3843e5485b04d4c996048870fb07355
DanielPortods <danielportods@gmail.com>
2020-10-18 07:23:22 -0300

    #130 Desenvolvendo actions e getters.

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import { createStore, createLogger } from 'vuex';
 import createPersistedState from "vuex-persistedstate";
+import Usuarios from '@/services/usuarios.js'
 
 const store = createStore({
     debug: true,
@@ -7,11 +8,19 @@ const store = createStore({
     state(){
         return {
             usuario: {},
-            token: ''
+            token: '',
+            swapPerfilLogin: false
         }        
     },
 
     mutations: {
+        SET_SWAP(state, payload){
+            if(this.debug){
+                console.log('Swap: ', payload)
+            }
+    
+            state.swapPerfilLogin = payload;
+        },
         SET_USUARIO(state, payload) {
             if(this.debug){
                 console.log('Novo usuário: ', payload)
@@ -46,15 +55,49 @@ const store = createStore({
     },
 
     actions: {
-        loginAction(){},
-        logoutAction(){},
-        validateSessionAction(){}
+        async loginAction({commit}, login){
+            await Usuarios.Login(login).then(response => {
+                commit('SET_USUARIO', response.data.user)
+                commit('SET_TOKEN', response.data.token)
+                window.location.href='http://localhost:8080/perfil'
+            }, erro => {
+                alert(erro.response.data.msg)
+            })
+        },
+
+        async validateSessionAction({commit}, payload){
+            
+            await Usuarios.Validar(payload).then(response => {
+                commit('SET_TOKEN', response.data.new_token)
+            }, erro => {
+                console.log('com err')
+                console.log(erro.response.data.msg)
+                window.location.href='http://localhost:8080/login'
+            }) 
+        }
     },
 
     getters: {
         getNome(state) {
             return state.usuario.user_name;
-        }        
+        },
+
+        getId(state) {
+            return state.usuario._id
+        },
+
+        getToken(state) {
+            if (state.token){
+                return state.token
+            }
+
+            return null
+        },
+
+        getSwap(state) {
+            return state.swapPerfilLogin
+        }
+
     }     
 })
 
