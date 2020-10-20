85f5abd81a9522463074b8bb76c227ee8a425b34
Emily Sousa <diassousa@yahoo.com.br>
2020-10-20 14:53:15 -0300

    #144 ajustando estilo da página de editar
    Co-authored-by: Denys <dennysrogeres@gmail.com>

--- src/views/Editar.vue
+++ src/views/Editar.vue
@@ -1,41 +1,37 @@
 <template>
 
     <Header/>
-        <section>
-             <div class="main">
+        <section>           
+            <form @submit.prevent="">
                 <h1>
                     Editar
                 </h1>
-           
-            <form @submit.prevent="">
-                    <div class="divInputs">
-                        <div class="divUser">
-                            <input style ="padrao" type="text" name="" placeholder="Nome">
-                            <input style ="padrao" type="text" name="" placeholder="Email">
-                        </div>
-
-                        <div class="divSenha">
-                            <span>
-                                <input style="senha" type="password" id="senha" name="" placeholder="Senha">
-                                <input style ="senha" type="password" id="novaSenha" name="" placeholder="Nova Senha">
-                            </span>
-                        </div>
-                    
+                <div class="divInputs">
+                    <div class="divUser">
+                        <input style ="padrao" type="text" name="" placeholder="Nome">
+                        <input style ="padrao" type="text" name="" placeholder="Email">
                     </div>
 
-                    <div class="divBotaoSalvar">
-                        <button type="button" onclick="window.location.href='http://localhost:8080/Perfil'">Salvar</button>
+                    <div class="divSenha">
+                        <span>
+                            <input style="senha" type="password" id="senha" name="" placeholder="Senha">
+                            <input style ="senha" type="password" id="novaSenha" name="" placeholder="Nova Senha">
+                        </span>
                     </div>
-
-                    <div class="divBotoes">
-                        <button type="button" v-on:click="fazerLogout" >Sair</button>
-                        <button type="button">Mudar senha</button>
-                        <button type=button>Excluir perfil</button>
-                        <button type=button>Sair da conta</button>
-                     </div>
-                </form>
+                
                 </div>
-            </section>
+
+                <div class="divBotaoSalvar">
+                    <button type="button" onclick="window.location.href='http://localhost:8080/Perfil'">Salvar</button>
+                </div>
+
+                <div class="divBotoes">
+                    <button type="button">Mudar senha</button>
+                    <button type=button style=color:red>Excluir perfil</button>
+                    <button type="button" v-on:click="fazerLogout" >Sair da conta</button>
+                 </div>
+            </form>
+        </section>
 
     <MenuBar/>
     
@@ -72,104 +68,92 @@ export default {
 
     section{
         width: 100%;
-        height: 100%;
+    }
+
+   form{
+    height: auto;
+    margin: 0 30px;
+    margin-top: 60px;
+
+
+    & .divInputs{
+        width: 100%;
+        height: 240px;
+        margin-top:  10px;
+
+        & .divUser{
+            input{
+                width: 100%;
+                height: 50px;
+
+                font-size: 14px;
+                border-bottom: 1px solid $colorCinza;
+                margin-bottom: 10px;
+            }
+        }
+        
+        & .divSenha{
+
+            height: 100%;
+
+            input{
+                box-shadow: 0 0 0 0;
+                margin-bottom: 10px;
+
+                width: 100%;
+                height: 50px;
+
+                font-size: 14px;
+                border-bottom: 1px solid $colorCinza;
+            }
+
+        }
+    }
+
+    & .divBotaoSalvar{
+        width: 100%;
+        height: 50px;
+        padding-top: 45px;
+
         display: flex;
-    }
 
-    .main {
-        margin-top: 60px;
-    }
-
-    button {
-        margin-left: 10px;
-    }
-
-       form{
-        height: 310px;
-
-        & .divInputs{
-            width: 100%;
-            height: 240px;
-            margin-top:50px;
-
-            & .divUser{
-                input{
-                    width: 100%;
-                    height: 50px;
-
-                    font-size: 14px;
-                    border-bottom: 1px solid $colorCinza;
-                    margin-bottom: 10px;
-                }
-            }
-            
-            & .divSenha{
-
-                height: 100%;
-
-                input{
-                    box-shadow: 0 0 0 0;
-                    border: 0 none;
-                    outline: 0;
-
-                    display: block;
-
-                    width: 100%;
-                    height: 50px;
-
-                    font-size: 14px;
-                    border-bottom: 1px solid $colorCinza;
-                }
-
-            }
-        }
-
-        & .divBotaoSalvar{
-            width: 100%;
+        & button{
             height: 50px;
+            width: 100%;
 
-            display: flex;
-
-            & button{
-                height: 50px;
-                width: 100%;
-
-                cursor: pointer;
-                font-size: 20px;
-                border: none;
-                border-radius: 25px;   
-                color: #ffffff;
-                background-color: $colorAzul;
-            }
-
-            & button:hover{
-                background-color: #060449;
-            }
+            cursor: pointer;
+            font-size: 20px;
+            border: none;
+            border-radius: 15px;   
+            color: $colorBranca;
+            background-color: $colorAzul;
         }
 
-        & .divBotoes{
-            width: 100%;
-            height: 50px;
-
-           // display:block;
-            & button{
-                height: 50px;
-                width: 100%;
-
-                cursor: pointer;
-                font-size: 20px;
-                border: none;
-                border-radius: 25px;   
-                color: $colorPreta;
-                background-color: $colorBranca;
-                border: 1px solid $colorPreta;
-            }
-
-            & button:hover{
-                background-color:$colorCinza;
-            }
+        & button:hover{
+            background-color: #060449;
         }
     }
+
+    & .divBotoes{
+        width: 100%;
+        height: 50px;
+
+        & button{
+            height: 50px;
+            width: 100%;
+
+            cursor: pointer;
+            font-size: 20px;
+            border-radius: 15px;
+            background-color: $colorBranca;
+            border: 1px solid $colorCinza;
+        }
+
+        & button:hover{
+            background-color:$colorCinza;
+        }
+    }
+}
 
 
 </style>
