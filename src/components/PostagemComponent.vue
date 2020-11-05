8b8d79b7ccc511740defaa962925a8c9ee4a655b
Bruno-Felix <balvesfelix@gmail.com>
2020-11-05 00:08:03 -0300

    #166 Readequando o uso dos atributos enviados por req.body no método apoiarPostagemMetodo - PostagemComponent e PostagemApoiadaComponent

--- src/components/PostagemComponent.vue
+++ src/components/PostagemComponent.vue
@@ -48,8 +48,8 @@ export default {
     data: function () {
         return{
             upsAtributos: {
-                fk_user_id: '',
-                fk_postage_id: ''
+                user_id: '',
+                postage_id: ''
             }
         }
     },
@@ -72,8 +72,8 @@ export default {
                         console.log("Usuário não logado")
                     }
                     else{
-                        this.upsAtributos.fk_user_id = this.$store.getters.getId
-                        this.upsAtributos.fk_postage_id = post_id
+                        this.upsAtributos.user_id = this.$store.getters.getId
+                        this.upsAtributos.postage_id = post_id
 
                         Postagem.apoiarUmaPostagem(this.upsAtributos).then(resposta => {
                             console.log(resposta)
