7c605298716e6fbd76655b6c9ab71f3fd7426447
Emily Sousa <diassousa@yahoo.com.br>
2020-11-11 17:48:21 -0300

    #174 adicionado arquivo de server e adicionando paths no static json

---
+++ server.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+var express = require('express')
+var path = require('path')
+var serveStatic = require('serve-static')
+
+var app = express()
+app.use(serveStatic(path.join(__dirname, 'dist')))
+
+var port = process.env.VUE_APP_API_URL || 8080
+app.listen(port)
+console.log('aqui' + port)
