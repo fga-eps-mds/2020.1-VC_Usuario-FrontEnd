fb50a5d3278b943e364a2d5614bcbe5e51564497
Emily Sousa <diassousa@yahoo.com.br>
2020-11-11 18:07:23 -0300

    #174 retirando file não utilizado

--- src/services/config.js
+++ src/services/config.js
@@ -1,7 +1,7 @@
-// import axios from 'axios'
+import axios from 'axios'
 
-// const http = axios.create({
-//     baseURL: process.env.VUE_APP_API_URL
-// })
+const http = axios.create({
+    baseURL: process.env.VUE_APP_API_URL
+})
 
-// export default http 
+export default http 
