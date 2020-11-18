1fb0ebb33124b3039224bd3c6fa17f9138e31a56
enzoggqs <eggqsaraiva@gmail.com>
2020-11-18 18:51:06 -0300

    #187 Adicionando sidebar em telas com resolução grande
    Co-authored-by: Bruno Felix <balvesfelix@gmail.com>

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,6 +7,21 @@
         <div class="divWordmark">
             <img src="../assets/WordMark_VC.png" onclick="window.location.href='/'"> 
         </div>
+
+        <div class="divSideBar">
+            <input type="checkbox" id=chec>
+            <label for="chec">
+                <img src="../assets/sidebar.png">
+            </label>
+            <nav class="SideBar">
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/criarpostagem">Criar Postagem</a></li>
+                    <li><a href="">Notícias</a></li>
+                    <li><a href="/Perfil">Perfil</a></li>
+                </ul>
+            </nav>
+        </div>
     </header>
 </template>
 
@@ -30,6 +45,10 @@ window.onscroll = function(){
 
 <style lang="scss" scoped>
     @import "../assets/stylesheets/pallete.scss";
+
+    @media only screen 
+    and (min-device-width : 0px) 
+    and (max-device-width : 1024px) {
 
     header{
         top: 0;
@@ -74,5 +93,111 @@ window.onscroll = function(){
             padding: 10px 10px 10px 0;
             margin-left: 30px;
         }
-    } 
+    }
+
+    .divSideBar{
+        display: none;
+    }
+
+    }
+
+    @media only screen 
+    and (min-device-width : 1024px) {
+
+    header{
+        top: 0;
+        position: fixed;
+        display: flex;
+
+        height: 45px;
+        width: 100%;
+        
+        transition: top 0.3s;
+        background-color: $colorAzul;
+    }
+
+    .divWordmark{
+        display: flex;
+        justify-content: center;
+        
+        height: 100%;
+        width: 100%;
+        margin: auto;
+        position:absolute;
+        z-index: -50;
+
+        & img{
+            height: 30px;
+            width: 60px;
+            padding: 5px 10px;
+            margin: auto;
+
+            display: block;
+            position: relative;
+            
+            cursor: pointer;
+        }
+    }
+
+    .divBotaoVoltar{
+        display: none;
+    }
+
+    .divSideBar{
+        position: fixed;
+        top: 7px;
+        left: 30px;
+        color: $colorBranca;
+        padding: 0px;
+        margin: 0px;
+        height: 100%;
+        z-index: -1;
+    }
+        & img{
+            height: 35px;
+            width: 40px;
+            color: $colorBranca
+        }
+
+        &ul {
+            list-style: none;
+        }
+
+        & a{
+            font-weight: bold;
+            color: $colorAzulEscuro;
+            font-size: 30px;
+            text-decoration: none;
+            display: block;
+            padding: 35px;
+            border-bottom: 1px solid $colorCinza;
+        }
+
+        & a:hover{
+            background-color: $colorAzulEscuro;
+            color: $colorBranca;
+        }
+
+        & input[type="checkbox"]
+        {
+            display: none;
+        }
+
+        & input[type="checkbox"]:checked ~ nav
+        {
+            transform: translateX(305px);
+            transition: 1s;
+        }
+
+        &nav[class="SideBar"]{
+            background-color: $colorBranca;
+            height: 100%;
+            border-right: 1px solid $colorCinza;
+            width: 305px;
+            position:absolute;
+            left: -340px           
+        }
+
+    }
+    
 </style>
