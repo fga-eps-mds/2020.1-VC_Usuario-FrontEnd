aef19442fb93c6e706b9db31c777f118c25eef11
Bruno-Felix <balvesfelix@gmail.com>
2020-11-30 18:33:01 -0300

    #187 Verificando se usuário está logado
    Co-authored-by: Emily Dias <diassousa@yahoo.com.br>

--- src/views/CriarPostagem.vue
+++ src/views/CriarPostagem.vue
@@ -73,6 +73,8 @@
 import HeaderComponent from '@/components/HeaderComponent.vue'
 import MenuBarComponent from '@/components/MenuBarComponent.vue'
 import Postagem from '@/services/postagensServices.js'
+
+import { useStore } from 'vuex'
 /* eslint-disable */
 
 export default {
@@ -95,6 +97,17 @@ export default {
                 post_type: 'false',
                 file: '',
                 post_author: ''
+            }
+        }
+    },
+
+    created: function() {
+        if( !useStore().getters.getSwap ){
+
+            const token = useStore().getters.getToken
+            if(!token){}
+            else {
+                useStore().dispatch('validateSessionAction', token)
             }
         }
     },
