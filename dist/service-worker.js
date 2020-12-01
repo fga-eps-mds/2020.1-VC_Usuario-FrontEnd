cbefa4c312725af46db18718487d08e259898330
Emily Sousa <diassousa@yahoo.com.br>
2020-12-01 13:50:42 -0300

    #202 indentando o service

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -11,16 +11,16 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
 importScripts(
-  "/precache-manifest.45942389c8710c819494bef675e512d4.js"
+  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
 );
 
-workbox.core.setCacheNameDetails({prefix: "teste-docker_front"});
+importScripts("/precache-manifest.45942389c8710c819494bef675e512d4.js");
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
+workbox.core.setCacheNameDetails({ prefix: "teste-docker_front" });
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
   }
 });
