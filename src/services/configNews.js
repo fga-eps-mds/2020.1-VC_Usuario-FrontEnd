abbdc0f4b275e18dc3caf43df6a2a877b0acf54d
Emily Sousa <diassousa@yahoo.com.br>
2020-12-01 02:46:24 -0300

    #202 tentando arrumar o service para aceitar http

--- src/services/configNews.js
+++ src/services/configNews.js
@@ -2,6 +2,10 @@ import axios from 'axios'
 
 const httpNews = axios.create({
     baseURL: process.env.VUE_APP_API_URL_NEWS, 
+    headers: {
+    Authorization: `Bearer ${localStorage.getItem('token')}`,
+    'X-Requested-With': 'XMLHttpRequest'
+  	}
 });
 
 export default httpNews; 
