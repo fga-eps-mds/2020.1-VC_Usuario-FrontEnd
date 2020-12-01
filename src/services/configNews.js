eca3df954b906de081c57ff75889783fc5ac6a93
Emily Sousa <diassousa@yahoo.com.br>
2020-12-01 02:53:33 -0300

    #202 retirando mudanças da config

--- src/services/configNews.js
+++ src/services/configNews.js
@@ -2,10 +2,6 @@ import axios from 'axios'
 
 const httpNews = axios.create({
     baseURL: process.env.VUE_APP_API_URL_NEWS, 
-    headers: {
-    Authorization: `Bearer ${localStorage.getItem('token')}`,
-    'X-Requested-With': 'XMLHttpRequest'
-  	}
 });
 
 export default httpNews; 
