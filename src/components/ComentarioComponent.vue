6fdfa116181f5ab182ffb5bea811f487beb3d338
Bruno-Felix <balvesfelix@gmail.com>
2020-12-01 21:15:38 -0300

    #192 Ajustando props e estilo do comentário - ComentarioComponent

--- src/components/ComentarioComponent.vue
+++ src/components/ComentarioComponent.vue
@@ -1,14 +1,33 @@
 <template>
-    
+    <div class="divComponentComentario">
+        <p>"{{UPC_description}}"</p>
+    </div>
 </template>
 
 <script>
 export default {
-    
+    name: 'comentarioBlock', 
+
+    props: {
+        _id: String,
+        fk_user_id: String,
+        fk_postage_id: String,
+        UPC_description: String,
+    },
 }
 </script>
 
 <style scoped lang="scss">
-@import "../assets/stylesheets/pallete.scss";
+    @import "../assets/stylesheets/pallete.scss";
 
+    .divComponentComentario {
+        height: auto;
+        width: 100%;
+
+        & p {
+            font-style: oblique;
+            text-align: justify;
+            color: $colorCinzaEscuro;
+        }
+    }
 </style>
